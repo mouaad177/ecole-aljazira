2ac242c7b8d5005229c91c7524ec1315bc8b1c3b
okoucha <mouaaad.kouch.17@gmail.com>
2026-09-02 19:45:26 +0200

    ajout clé api insta

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 document.addEventListener('DOMContentLoaded',()=>{const b=document.querySelector('.menu-toggle'),n=document.querySelector('.nav');if(b&&n)b.addEventListener('click',()=>n.classList.toggle('open'));document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>n&&n.classList.remove('open')));const y=document.getElementById('year');if(y)y.textContent=new Date().getFullYear();});
+async function loadInstagramPosts() {
+    try {
+        const response = await fetch(
+            "https://hidden-hat-b103el-jazira-instagram-api.mouaad-kouch.workers.dev/instagram"
+        );
+
+        const data = await response.json();
+
+        console.log("Réponse de l'API Instagram :", data);
+
+    } catch (error) {
+        console.error("Erreur API Instagram :", error);
+    }
+}
+
+loadInstagramPosts();
